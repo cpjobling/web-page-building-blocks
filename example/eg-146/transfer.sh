2cd4acf276d4d37e6fa4de8609206a3b161a6b43
Chris Jobling <c.p.jobling@swansea.ac.uk>
2012-05-18 09:51:54 +0100

    Changed link

--- example/eg-146/transfer.sh
+++ example/eg-146/transfer.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo 'user@cpjobling.org.uk:~/public_html/eg-146> ls -lR' > dir.txt
-ls -lR >> dir.txt
+ls -lR ~/dev/web-page-building-blocks/example/eg-146 >> dir.txt
 
-rsync -avrz --delete --exclude=transfer.sh . cpjoblin@cpjobling.me:public_html/eg-146
+rsync -avrz --delete --exclude=transfer.sh . cjobling@eng-hope.swan.ac.uk:../../var/www/eg-146/website
